81f6b6270c35d553590d534b692e29b1d1957a1f
marlon jaramillo <marlonjaramillo@MacBook-Pro-de-Marlon.local>
2024-06-29 16:30:13 -0500

    Commit - Correccion Estilos InfoUs

--- src/components/InfoUs.jsx
+++ src/components/InfoUs.jsx
@@ -4,6 +4,7 @@ import { doc, getDoc } from "firebase/firestore";
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
 import { db } from "../firebase/config";
 import animatedGif2 from '/src/assets/gifLogo3.gif';
+import { EyeSlashIcon, FaceFrownIcon } from '@heroicons/react/24/solid';
 
 const InfoUs = () => {
 
@@ -70,79 +71,86 @@ const InfoUs = () => {
 
     return (
         <>
-        <div className="card-gen-infoUs">
-            <h1>Información del Usuario</h1>
-            <div className="card-infoUs">
-                <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-                    <div style={{ height: "60vh", width: "100%", borderRadius: "15px", overflow: "hidden" }}>
+            <div className="card-gen-infoUs">
+                <h1>Información del Usuario</h1>
+                {error ? (
+                    <>
+                        <FaceFrownIcon style={{ width: "50px" }}/>
+                        <p style={{ color: "red", marginTop: "1rem", paddingLeft: "1rem", paddingRight: "1rem" }}>{error}</p>
+                        <div style={{ marginBottom: "3rem" }} className="button-container">
+                            <button className="exit-button" onClick={handleExit}>Salir</button>
+                        </div>
+                    </>
+                ) : (
+                    <div className="card-infoUs">
+                        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
                         {userData ? (
                             userData.PodevWeb ? (
-                                <Map
-                                    defaultZoom={15}
-                                    defaultCenter={userPosition}
-                                    mapId={import.meta.env.VITE_MAP_ID}
-                                >
-                                    <AdvancedMarker
-                                        position={userPosition}
-                                        onClick={() => setOpen(true)}
-                                    />
-                                    {open && (
-                                        <InfoWindow
-                                            position={userPosition}
-                                            onCloseClick={() => setOpen(false)}
+                            
+                                    <div style={{ height: "60vh", width: "100%", borderRadius: "15px", overflow: "hidden" }}>
+                                        <Map
+                                            defaultZoom={15}
+                                            defaultCenter={userPosition}
+                                            mapId={import.meta.env.VITE_MAP_ID}
                                         >
-                                            <div className="InfoWindow">
-                                                <div className="gif-container">
-                                                    <img src={animatedGif2} className="animated-gif-infoUs" alt="Animated GIF" />
-                                                </div>
-                                                <p><strong>Usuario:</strong> {userData.nombre} {userData.apellido}</p>
-                                                <p><strong>Dirección:</strong> {address}</p>
-                                            </div>
-                                        </InfoWindow>
-                                    )}
-                                </Map>
+                                            <AdvancedMarker
+                                                position={userPosition}
+                                                onClick={() => setOpen(true)}
+                                            />
+                                            {open && (
+                                                <InfoWindow
+                                                    position={userPosition}
+                                                    onCloseClick={() => setOpen(false)}
+                                                >
+                                                    <div className="InfoWindow">
+                                                        <div className="gif-container">
+                                                            <img src={animatedGif2} className="animated-gif-infoUs" alt="Animated GIF" />
+                                                        </div>
+                                                        <p><strong>Usuario:</strong> {userData.nombre} {userData.apellido}</p>
+                                                        <p><strong>Dirección:</strong> {address}</p>
+                                                    </div>
+                                                </InfoWindow>
+                                            )}
+                                        </Map>
+                                    </div>
+                                
                             ) : (
                                 <>
-                                    <p>Loading...</p>
-                                    <p style={{ color: "red" }}>
-                                        El usuario no tiene activa la función para visualizar su ubicación.
-                                    </p>
+                                    <div>
+                                        <EyeSlashIcon style={{ width: "50px"}}/>
+                                        <p style={{ color: "red", paddingLeft: "1rem", paddingRight: "1rem"}}>
+                                            El usuario no tiene activa la función para visualizar su ubicación.
+                                        </p>
+                                    </div>
                                 </>
                             )
                         ) : (
                             <p>Loading...</p>
                         )}
-                    </div>
-                </APIProvider>
-                <div className="infoUs">
-                    {error ? (
-                        <p style={{ color: "red" }}>{error}</p>
-                    ) : (
-                        userData && (
-                            <div>
-                                <h2 style={{ textAlign: "center" }}>
-                                    <strong>{userData.nombre} {userData.apellido}</strong>
-                                </h2>
-                                <p><strong>Celular:</strong> {phoneWithCountryCode}</p>
-                                <p style={{ paddingBottom: "2rem" }}><strong>Email:</strong> {userData.email}</p>
-                                <hr/>
-                                <h2 style={{ textAlign: "center", paddingTop: "1rem" }}>
-                                    <strong>Contacto de emergencia</strong>
-                                </h2>
-                                <p><strong>Nombre:</strong> {userData.nombreContacto}</p>
-                                <p><strong>Número:</strong> {userData.numeroContacto}</p>
-                                {/* <p><strong>Latitud:</strong> {userData.latitud}</p>
-                                <p><strong>Longitud:</strong> {userData.longitud}</p>
-                                <p><strong>PodevWeb:</strong> {userData.PodevWeb ? "Sí" : "No"}</p> */}
+                        </APIProvider>
+                        <div className="infoUs">
+                            {userData && (
+                                <div>
+                                    <h2 style={{ textAlign: "center" }}>
+                                        <strong>{userData.nombre} {userData.apellido}</strong>
+                                    </h2>
+                                    <p><strong>Celular:</strong> {phoneWithCountryCode}</p>
+                                    <p style={{ paddingBottom: "2rem" }}><strong>Email:</strong> {userData.email}</p>
+                                    <hr />
+                                    <h2 style={{ textAlign: "center", paddingTop: "1rem" }}>
+                                        <strong>Contacto de emergencia</strong>
+                                    </h2>
+                                    <p><strong>Nombre:</strong> {userData.nombreContacto}</p>
+                                    <p><strong>Número:</strong> {userData.numeroContacto}</p>
+                                </div>
+                            )}
+                            <div className="button-container">
+                                <button className="exit-button" onClick={handleExit}>Salir</button>
                             </div>
-                        )
-                    )}
-                    <div className="button-container">
-                        <button className="exit-button" onClick={handleExit}>Salir</button>
+                        </div>
                     </div>
-                </div>
+                )}
             </div>
-        </div>
         </>
     )
 }
